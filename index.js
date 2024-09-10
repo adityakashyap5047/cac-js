6c227b313f6d1058977381815d68a29bc53c30e1
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-09-10 23:21:22 +0530

    Starting the js series

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log("Welcome to the Brain Games!");
+console.log("Welcome to the Series of JavaScript");
+
+console.log("There is not as such difference between the .txt and .js file. Both are text files and we can treat them as the source code. The only difference is that which file can be executed by injecting it under a software i.e. .js file is used for JavaScript code and .txt file is used for text data and not for the code execution.");
